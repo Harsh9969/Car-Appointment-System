c6f6bb288ddfc5e6967a1aa28a1310814507f66b
Harsh9969 <harshpatel9969.hp@gmail.com>
2021-11-08 02:30:07 -0500

    Create db.php

---
+++ user/db.php
@@ -0,0 +1,10 @@
+<?php
+	$host="localhost";
+	$user="root";
+	$pass="";
+	$dbname="project";
+	$con=mysqli_connect($host,$user,$pass,$dbname)or die("unable to connect");
+	//echo "successfully connected";
+	//mysqli_connect("localhost","root","","project");
+	
+?>
