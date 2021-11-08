456b652fa3e22c8ec256ff557f99b51e5595cd02
Harsh9969 <harshpatel9969.hp@gmail.com>
2021-11-08 02:17:44 -0500

    Create servicecount.php

---
+++ admin/servicecount.php
@@ -0,0 +1,154 @@
+<?php session_start()?>
+
+<!doctype html>
+<html class="no-js" lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title></title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="shortcut icon" type="image/png" href="assets/images/icon/favicon.ico">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/themify-icons.css">
+    <link rel="stylesheet" href="css/metisMenu.css">
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/slicknav.min.css">
+    <!-- amchart css -->
+    <link rel="stylesheet" href="https://www.amcharts.com/lib/3/plugins/export/export.css" type="text/css" media="all" />
+    <!-- others css -->
+    <link rel="stylesheet" href="css/typography.css">
+    <link rel="stylesheet" href="css/default-css.css">
+    <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="css/responsive.css">
+    <!-- modernizr css -->
+    <script src="js/vendor/modernizr-2.8.3.min.js"></script>
+	<script src="js/jquery.js"></script>
+
+	
+	</head>
+
+
+
+
+<body>
+  
+    <!-- preloader area start -->
+    <div id="preloader">
+        <div class="loader"></div>
+    </div>
+    <!-- preloader area end -->
+    <!-- page container area start -->
+    <div class="page-container">
+        <!-- sidebar menu area start -->
+        <?php include('siderbar.php');?>
+	
+        <!-- sidebar menu area end -->
+        <!-- main content area start -->
+		<div class="main-content">
+            <!-- header area start -->
+            <?php include('header.php');?>
+            <!-- header area end -->
+			
+				<div class="row">
+						<div class="col-md-12">
+						
+						<h2>Higest services</h2>
+						</div>
+						
+						<!--<div class="row">-->
+						<div class="col-md-12">
+							<div id="HTMLtoPDF">
+							<div class="table-responsive table-no-card m-b-30"><br>
+                             <form method="post">
+							 <table class="table table-borderless table-striped table-earning" style="margin:0px auto;"> 
+
+
+
+						<tr>
+							<th>Service id</th>
+							<th>Service Name</th>
+							<th>Total</th>
+						</tr>
+						
+							<?php
+								include "db.php";
+								$sql="SELECT sm.sid,s4.sname,count(*) FROM appoinment_type ap,image_s4 s4,service_master sm where sm.sid=s4.sid and ap.sid=sm.smid GROUP BY sm.sid ";
+								$res=mysqli_query($con,$sql);
+								while($row=mysqli_fetch_array($res))
+								{
+									$id=$row['sid'];
+									echo '<tr><td>'.$row['sid'].'</td>
+										<td>'.$row['sname'].'</td>
+										<td>'.$row[2].'</td></tr>';
+								}
+							
+							?>
+							
+						
+						
+							</table>
+							</form>
+                            </div>
+							</div>
+							<button class="btn btn-warning"><a href="#" onclick="HTMLtoPDF()">Download PDF</a></button>
+						</div>		
+						
+						<!--<div id="HTMLtoPDF">-->
+						<div class="col-md-12">
+							<div class="table-responsive table-no-card m-b-30"><br>
+							<table>
+								
+								<tr>
+								<td><span class="state"></span></td>
+								
+								</tr>
+							</table>
+							</div>
+						</div>
+				<!--</div>-->
+				</div>
+				
+        </div>
+		<!-- main content area end -->
+    </div>
+     <!-- page container area end -->          
+               
+   
+                    
+               
+        
+        
+
+		
+    <!-- jquery latest version -->
+    <script src="js/vendor/jquery-2.2.4.min.js"></script>
+    <!-- bootstrap 4 js -->
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/owl.carousel.min.js"></script>
+    <script src="js/metisMenu.min.js"></script>
+    <script src="js/jquery.slimscroll.min.js"></script>
+    <script src="js/jquery.slicknav.min.js"></script>
+
+    <!-- start chart js -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.2/Chart.min.js"></script>
+    <!-- start highcharts js -->
+    <script src="https://code.highcharts.com/highcharts.js"></script>
+    <!-- start zingchart js -->
+    <script src="https://cdn.zingchart.com/zingchart.min.js"></script>
+    
+    <!-- all line chart activation -->
+    <script src="js/line-chart.js"></script>
+    <!-- all pie chart -->
+    <script src="js/pie-chart.js"></script>
+    <!-- others plugins -->
+    <script src="js/plugins.js"></script>
+    <script src="js/scripts.js"></script>
+	<script src="js/jspdf.js"></script>
+	<script src="js/jquery-2.1.3.js"></script>
+	<script src="js/pdfFromHTML.js"></script>
+</body>
+
+</html>
