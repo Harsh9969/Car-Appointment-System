8ceed27d7a3cc5f61e0c3501b8e4d4c4c4733c85
Harsh9969 <harshpatel9969.hp@gmail.com>
2021-11-08 02:17:54 -0500

    Create siderbar.php

---
+++ admin/siderbar.php
@@ -0,0 +1,49 @@
+ <div class="sidebar-menu">
+            <div class="sidebar-header">
+                <div class="logo">
+                    <a href=""><img src="images/logo1.jpg" alt="logo"></a>
+                </div>
+            </div>
+            <div class="main-menu">
+                <div class="menu-inner">
+                    <nav>
+                        <ul class="metismenu" id="menu">
+                            <li class="active">
+                                <a href="index.php" aria-expanded="true"><i class="ti-dashboard"></i><span>dashboard</span></a>
+                               
+                            </li>
+                            <li>
+                                <a href="javascript:void(0)" aria-expanded="true"><i class="ti-hummer"></i><span>Services</span></a>
+                                <ul class="collapse">
+                                    <li><a href="services.php">Add services type</a></li>
+									<li><a href="service_detail.php">Add services details</a></li>
+                                    <!--<li><a href="product.php">Add Spares</a></li>-->
+									<li><a href="carmake.php">Add Car Model</a></li>
+									<li><a href="service_master.php">Service_master</a></li>
+                                </ul>
+                            </li>
+							 <li>
+                                <a href="javascript:void(0)" aria-expanded="true"><i class="ti-hummer"></i><span>Manage</span></a>
+                                <ul class="collapse">
+                                    <li><a href="manage_user.php">Manage User</a></li>
+                                    <li><a href="manage_app.php">Manage Appointment</a></li>
+                                </ul>
+                            </li>
+							<li>
+                                <a href="feedback.php" aria-expanded="true"><i class="ti-agenda"></i><span>FeedBack</span></a>
+                               
+                            </li>
+							<li>
+                                <a href="" aria-expanded="true"><i class="ti-hummer"></i><span>Report</span></a>
+                                <ul class="collapse">
+                                    <li><a href="report.php">customer</a></li>
+                                    <li><a href="sevicereport.php">As per servicewise</a></li>
+									<li><a href="servicecount.php">highest service</a></li>
+									<li><a href="dwreport.php">datewise</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        </div>
